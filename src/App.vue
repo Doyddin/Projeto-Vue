06783ac8cd309e48a6821716b8c33d6ae7fec4e8
unknown <Doyddin>
2024-04-16 19:48:27 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import {ref} from 'vue'
+</script>
+
+<template>
+  <!-- Menu Booststrap -->
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+      <router-link class="navbar-brand" to="/">Pokedex</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <!-- <a class="nav-link active" aria-current="page" href="#">Início</a> -->
+            <router-link class="nav-link" aria-current="page" to="/">Início</router-link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="#">Sobre</a> -->
+            <router-link class="nav-link" to="/about">Sobre</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </nav>
+
+  <div class="main">
+    <router-view></router-view>
+  </div>  
+
+
+  <footer class="bg-dark text light">
+    <div>
+      Unimar | ADS | {{ new Date().getFullYear()}}
+    </div>
+  </footer>
+</template>
+
+<style>
+
+footer{
+  position: fixed;
+  bottom: 0;
+  width:100%;
+  display: flex;
+  align-items: center;
+  justify-cointent: center;
+  height: 40px;
+}
+
+</style>
+
+
